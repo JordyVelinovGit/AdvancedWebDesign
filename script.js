0c6d81bd5bc77e353ff5d2782c52c06bd810c409
JordyVelinov <jordy@velinovi.com>
2022-09-13 10:32:38 -0400

    Created Navbar

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
-// function handleClick(event)
-// {
-
-// }
-
-// const handleClick2 = function(event)
-// {
-
-// }
-
-// const handleClick3 = event =>
-// {
-
-// }
-
-const button = document.getElementById("convertButton")
-button.addEventListener("click", (_) => {
-    const circle = document.querySelector(".round")
-    circle.style.display = "none"
-
-    // setTimeout(() => {
-    //     circles.forEach(circle => {
-    //         circle.classList.remove("growAndShrink")
-    //     })
-    // }, 1000)
-})
-
-
-
-// const handleClick = (element) =>
-// {
-//     element.style.width = element.style.width ?
-//         +(element.style.width.replace("px", "")) + 100 + "px" : "100px"
-//     console.log(element)
-// }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
